2b1cccf4c837c27eedda4fccfdb97e6be8eb5c5b
황주빈 <hjb06288@gmail.com>
2023-11-27 15:42:41 +0900

    시저암호
    stringbuilder 사용

--- src/main/java/org/level1/Solution2.java
+++ src/main/java/org/level1/Solution2.java
@@ -4,27 +4,26 @@ package org.level1;
 //시저암호
 public class Solution2 {
     public String solution(String s, int n) {
-        String answer = "";
 
-        int a = 0 ;
-        for(int i = 0 ; i< s.length(); i++){
-            if(s.charAt(i)==' '){
-                answer+=s.charAt(i);
-                continue;
+        StringBuilder str = new StringBuilder();
+
+        for (char x : s.toCharArray()) {
+
+            //x가 알파벳이 아닐 때는 공백 그대로 str에 추가
+            if (!Character.isAlphabetic(x)) {
+                str.append(x);
+                System.out.println(x);
+            } else {
+                //알파벳을 0부터 시작하게 만들기
+                char offset = Character.isUpperCase(x) ? 'A' : 'a';
+                //몇번째인지 확인
+                int position = x - offset;
+                //알파벳 길이보다 더 초과하게 되면 처음으로 돌아가기 위해서 알파벳 개수만큼 나눈 나머지를 취하기
+                position = (position+n)%26;
+                //원래 알파벳 아스키 코드로 복귀후 char 로 만들기
+                str.append((char)(position+offset));
             }
-
-            a = s.charAt(i)+n;
-
-            if(s.charAt(i)<91 && a>90){
-                a-=26;
-            }
-
-            if (a > 122){
-                a-=26;
-            }
-
-            answer+=(char)a;
         }
-        return answer;
+        return str.toString();
     }
 }
